00b3a1c189e397d594768fdf23d6ac6a81eaeef1
Ankur <ankur93jain@gmail.com>
2018-08-02 15:09:35 +0530

    Merge conflict Resolve.

--- src/main/java/com/design/pattern/singleton/SingletonClass.java
+++ src/main/java/com/design/pattern/singleton/SingletonClass.java
@@ -1,6 +1,6 @@
 package com.design.pattern.singleton;
 
-// This is a test Singleton class implementation
+// This is a test Singleton class implementation using eager and lazy initialization process
 public class SingletonClass {
 
 }
